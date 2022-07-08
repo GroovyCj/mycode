1f9926e0012660df5c1a8644806ae20d54bdc763
Cory Hairston <GroovyCj@users.noreply.github.com>
2022-07-08 14:59:14 +0000

    added final project

--- flaskapi/alta3research-flask01.py
+++ flaskapi/alta3research-flask01.py
@@ -6,15 +6,15 @@ from flask import url_for
 from flask import request
 from flask import render_template
 import json
-
+from flask import jsonify
 
 app = Flask(__name__)
 
 goodans = "otacon"
 
-@app.route("/")
-def start():
-    return render_template("landingpage.html")
+@app.route("/<name>")
+def start(name):
+    return render_template("landingpage.html",name=name )
 
 
 @app.route("/answer", methods=["POST"])
@@ -23,10 +23,7 @@ def rightornot():
         if request.form.get("ans")== "otacon":
             return redirect(url_for("correct"))
         else:
-            return redirect(url_for("start"))
-
-
-
+            return redirect(url_for("start",name="captain failure"))
 
 
 
@@ -36,6 +33,28 @@ def correct():
         return "You are correct!"
     
 
+metaldata = [{
+    "name" : "Otacon",
+    "realname" : "Hal Emmerich",
+    "friend" : "Solid Snake",
+    "first appearance" : "Metal Gear Solid"},
+    { "name" : " Solid Snake",
+      "realname" : "David",
+      "friend" : "Otacon",
+      "first appearance" : "Metal Gear Solid"}]
+
+@app.route("/metal", methods=["GET", "POST"])
+def charShow():
+    if request.method == "POST":
+        data = request.json
+        if data:
+           data= json.loads(data)
+           name = data["name"]
+           realname = data["realname"]
+           friend = data["friend"]
+           firstappearance  = data["first appearance"]
+           metaldata.append({"name" : name, "realname":realname, "friend":friend, "first appearance": firstappearance})
+    return jsonify(metaldata)
 
 
 
